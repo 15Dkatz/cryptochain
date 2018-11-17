2061f76aa2401e182218bc024c1b5672b6715d5f
David Katz <15Dkatz@shcp.edu>
2018-11-17 14:03:25 -0800

    Request document.location.origin

--- client/src/components/Blocks.js
+++ client/src/components/Blocks.js
@@ -6,7 +6,7 @@ class Blocks extends Component {
   state = { blocks: [] };
 
   componentDidMount() {
-    fetch('http://localhost:3000/api/blocks')
+    fetch(`${document.location.origin}/api/blocks`)
       .then(response => response.json())
       .then(json => this.setState({ blocks: json }));
   }
